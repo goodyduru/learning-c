5127ec9d02b2aa4f520f8719f111bbddca23d7ce
Duru Goodness <goodyduru@gmail.com>
2020-07-03 16:33:24 +0100

    Add celsius to fahrenheit conversion

--- Chapter 1/temperature.c
+++ Chapter 1/temperature.c
@@ -15,4 +15,12 @@ int main() {
 		printf("%3.0f %6.1f\n", fahr, celsius);
 		fahr += step;
 	}
+
+	celsius = lower;
+	printf("%s\t%s\n", "Cel", "Fahr");
+	while ( celsius <= upper ) {
+		fahr = (9.0*celsius/5.0) + 32.0;
+		printf("%3.0f %6.1f\n", celsius, fahr);
+		celsius += step;
+	}
 }
