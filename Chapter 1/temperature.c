8b44e981ded745005ca14a3670696fd2e8f33c0d
Duru Goodness <goodyduru@gmail.com>
2020-07-03 16:20:51 +0100

    Add headings

--- Chapter 1/temperature.c
+++ Chapter 1/temperature.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main() {
-	int fahr, celsius;
-	int lower, upper, step;
+	float fahr, celsius;
+	float lower, upper, step;
 
 	lower = 0;
 	upper = 300;
 	step = 20;
 
 	fahr = lower;
+	printf("%s\t%s\n", "Fahr", "Cel");
 	while ( fahr <= upper ) {
-		celsius = 5 * (fahr-32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
 		fahr += step;
 	}
 }
