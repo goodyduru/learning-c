965afe18859c5128ec1ee560c4a068ed3de53aa1
Duru Goodness <goodyduru@gmail.com>
2020-11-19 19:22:50 +0100

    Simplify type conversion

--- Chapter 2/ctype.c
+++ Chapter 2/ctype.c
@@ -20,20 +20,19 @@ int my_lower(int c) {
 
 int my_htoi(char s[]) {
 	int i, n;
+	char lower_case;
 	n = 0;
 	for (i = 0; (s[i] >= '0' && s[i] <= '9') || (s[i] >= 'a' && s[i] <= 'f') 
 			|| (s[i] >= 'A' && s[i] <= 'F') || (s[i] == 'x' || s[i] == 'X'); i++ ) {
-		if (s[i] == 'x' || s[i] == 'X'){
+		lower_case = (char)my_lower(s[i]);
+		if (lower_case == 'x'){
 	       		n = 0;
 	       	}
-	       	else if (s[i] >= 'a' && s[i] <= 'f') {
-			n = 16 * n + (s[i] - 'a' + 10);
-		}
-		else if (s[i] >= 'A' && s[i] <= 'F') {
-			n = 16 * n + (s[i] - 'A' + 10);
+	       	else if (lower_case >= 'a' && lower_case <= 'f') {
+			n = 16 * n + (lower_case - 'a' + 10);
 		}
 		else {
-			n = 16 * n + (s[i] - '0');
+			n = 16 * n + (lower_case - '0');
 		}
 	}
 	return n;
