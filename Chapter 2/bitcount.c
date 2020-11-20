7ae7d19917f621fa8c1a3f09bc44e07f881c86e6
Duru Goodness <goodyduru@gmail.com>
2020-11-20 17:37:28 +0100

    Complete exercise 2-9

---
+++ Chapter 2/bitcount.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* bitcount function
+ * It uses the fact that x & x-1 will remove the rightmost bit in x.
+ * With that we can count all the 1 bits in an integer
+ * For example, with number 5 which is 101 in a 2 complement number system.
+ * 101 & 100 will be 100, in the second iteration which will result in 100 & 11
+ * will result in 0. This only requires 2 iteration which incidentally is the 
+ * number of 1-bit in 101.
+ */
+int bitcount(int x) {
+	int n = 0;
+	while (x != 0) {
+		x &= (x - 1);
+		n++;
+	}
+	return n;
+}
+
+int main() {
+	int j = bitcount(10);
+	printf("%d", j);
+}
